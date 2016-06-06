44846472c4a28fb486c3a92c0f8cbe49f6b57353
pmoranc <pablomoranc@gmail.com>
2016-06-05 22:07:42 -0700

    se agrego moment.js

--- scripts/services/time.js
+++ scripts/services/time.js
@@ -1,4 +1,4 @@
-/* scripts/services/time.js */
+cripts/services/time.js */
     
 (function() {
     
@@ -6,24 +6,60 @@
 
     angular
         .module('timeTracker')
-        .factory('timex', time);
+        .factory('time', time);
 
         function time($resource) {
+
             // ngResource call to our static data
             var Time = $resource('data/time.json');
 
+            // $promise.then allows us to intercept the results of the 
+            // query so we can add the loggedTime property
             function getTime() {
-                // $promise.then allows us to intercept the results
-                // which we will use later
                 return Time.query().$promise.then(function(results) {
+                    angular.forEach(results, function(result) {
+
+                        // Add the loggedTime property which calls 
+                        // getTimeDiff to give us a duration object
+                        result.loggedTime = getTimeDiff(result.start_time, result.end_time);
+                    });
                     return results;
                 }, function(error) { // Check for errors
                     console.log(error);
                 });
             }
 
+            // Use Moment.js to get the duration of the time entry
+            function getTimeDiff(start, end) {
+                var diff = moment(end).diff(moment(start));
+                var duration = moment.duration(diff);
+                return {
+                    duration: duration
+                }
+            }
+            
+            // Add up the total time for all of our time entries
+            function getTotalTime(timeentries) {
+                var totalMilliseconds = 0;
+
+                angular.forEach(timeentries, function(key) {
+                    totalMilliseconds += key.loggedTime.duration._milliseconds;
+                });
+
+                // After 24 hours, the Moment.js duration object
+                // reports the next unit up, which is days.
+                // Using the asHours method and rounding down with
+                // Math.floor instead gives us the total hours
+                return {
+                    hours: Math.floor(moment.duration(totalMilliseconds).asHours()),
+                    minutes: moment.duration(totalMilliseconds).minutes()
+                }
+            }
+
             return {
                 getTime: getTime,
+                getTimeDiff: getTimeDiff,
+                getTotalTime: getTotalTime
             }
         }
 })();
